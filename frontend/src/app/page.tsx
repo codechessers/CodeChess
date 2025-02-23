0a4d7e6ca7a7a5c7f052d0781db5592b7f9dfe16
shreyansh-web <shreyansh487@gmail.com>
2025-02-23 23:01:19 +0530

    feat: add how it works

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,6 @@ import { WavyBackground } from "@/components/ui/wavy-background";
 export default function Home() {
   return (
     <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-col max-h-screen font-cairoli bg-black [background-image:radial-gradient(circle_500px_at_50%_420px,#38461b,transparent),linear-gradient(rgba(178,255,20,0.05)_1px,transparent_1px),linear-gradient(90deg,rgba(178,255,20,0.05)_1px,transparent_1px)] [background-size:100%_100%,50px_50px,50px_50px]">
-
       <Header />
       <MaxWidthWrapper>
         <div className="relative flex items-center justify-center">
@@ -20,7 +19,9 @@ export default function Home() {
           <Features />
         </WavyBackground>
       </div>
-      <HowItWorks />
+      <MaxWidthWrapper>
+        <HowItWorks />
+      </MaxWidthWrapper>
     </div>
   );
 }
